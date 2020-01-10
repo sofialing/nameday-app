6cc0825f5e32fc860f17b3854ad5e8f9cb43e020
Sofia Ling <sofialing@live.se>
2020-01-10 17:54:41 +0100

    Added search functions

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,3 +2,34 @@
  *
  *
  */
+
+const nameSearch = document.querySelector('#name-search');
+const dateSearch = document.querySelector('#date-search');
+
+const handleError = e => {
+	console.log(e);
+};
+
+const handleResult = res => {
+	console.log('got result:', res);
+};
+
+nameSearch.addEventListener('submit', e => {
+	e.preventDefault();
+	const name = nameSearch.name.value.trim();
+
+	searchByName(name)
+		.then(handleResult)
+		.catch(handleError);
+});
+
+dateSearch.addEventListener('submit', e => {
+	e.preventDefault();
+	const date = dateSearch.date.value.trim();
+	const month = new Date(date).getMonth() + 1;
+	const day = new Date(date).getDate();
+
+	searchByDate(month, day)
+		.then(handleResult)
+		.catch(handleError);
+});
