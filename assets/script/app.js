54944e058cfdec1b32fb047f219c46b9a3c4a272
Sofia Ling <sofialing@live.se>
2020-01-12 11:45:32 +0100

    Code refactoring and added comments

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,10 +1,9 @@
 /*
- *
- *
+ * Name day search
  */
-
 const nameSearch = document.querySelector('#name-search');
 const dateSearch = document.querySelector('#date-search');
+const resultEl = document.querySelector('#nameday');
 
 // Render a list of availabe countries
 const renderCountryList = () => {
@@ -69,28 +68,40 @@ const renderCountryList = () => {
 
 	// Append countries as options to select element
 	countryList.forEach(country => {
-		document.querySelector(
-			'#country'
-		).innerHTML += `<option value="${country.code}-${country.name}">${country.name}</option>`;
+		const html = `<option value="${country.code}-${country.name}">${country.name}</option>`;
+		document.querySelector('#country').innerHTML += html;
 	});
 };
 
 // Handle error messages and render to page
-const handleError = message => {
-	const html = `<div class="alert alert-warning" role="alert">${message}</div>`
-	document.querySelector('#nameday').innerHTML = html;
+const renderError = message => {
+	resultEl.innerHTML = `<div class="alert alert-warning" role="alert">${message}</div>`;
 };
 
-// Handle search results for name search, set variables
+// Handle search results for name search, format and set variables
 const handleNameResult = (res, search) => {
+	// Check if search got no results & render message
 	if (!res.results.length) {
-		handleError('No name day found.');
+		renderError('No name day found.');
 		return;
 	}
 
-	const name = res.results[0].name.split(', ').filter(name => name.toLowerCase() === search.toLowerCase());
-	const nameList = res.results[0].name.split(', ').filter(name => name.toLowerCase() !== search.toLowerCase());
-	const date = moment().month(res.results[0].month - 1).date(res.results[0].day).format('dddd, MMMM Do');
+	// Remove searched name from string of names (if multiple names)
+	const nameList = res.results[0].name
+		.split(', ')
+		.filter(name => name.toLowerCase() !== search.toLowerCase())
+		.join(', ');
+	const name = res.results[0].name
+		.split(', ')
+		.filter(name => name.toLowerCase() === search.toLowerCase());
+
+	// Format date of the name day
+	const date = moment()
+		.month(res.results[0].month - 1)
+		.date(res.results[0].day)
+		.format('dddd, MMMM Do');
+
+	// Get country name
 	const country = res['country name'];
 
 	renderNameSearch({ name, nameList, date, country });
@@ -98,22 +109,28 @@ const handleNameResult = (res, search) => {
 
 // Render results of search by specific name
 const renderNameSearch = ({ name, nameList, date, country }) => {
-	nameList = nameList.length ? nameList.join(', ') : 'no other names.'
-
 	const html = `
 		<div class="card">
 			<div class="card-body text-center">
 				<h2 class="font-weight-bold">${name}</h2>
 				<p>${date} (${country})</p>
-				<p class="text-muted">More on this day: ${nameList}</p>
+				<p class="text-muted">
+					More on this day: 
+					${nameList.length ? nameList : 'no more this day.'}
+				</p>
 			</div>
-		</div>`
-	document.querySelector('#nameday').innerHTML = html;
-}
+		</div>`;
+	resultEl.innerHTML = html;
+};
 
 // Render results of search by specific date
 const renderDateSearch = (res, country) => {
-	const date = moment().month(res.data[0].dates.month - 1).date(res.data[0].dates.day).format('dddd, MMMM Do');
+	// Format date of the name day
+	const date = moment()
+		.month(res.data[0].dates.month - 1)
+		.date(res.data[0].dates.day)
+		.format('dddd, MMMM Do');
+
 	const html = `
 		<div class="card">
 			<div class="card-body text-center">
@@ -121,33 +138,43 @@ const renderDateSearch = (res, country) => {
 				<p>${res.data[0].namedays[country.slice(0, 2)]}</p>
 				<p>(${country.slice(3)})</p>
 			</div>
-		</div>`
-	document.querySelector('#nameday').innerHTML = html;
-}
+		</div>`;
+	resultEl.innerHTML = html;
+};
 
 // Search by specific name when form is being submitted
-nameSearch.addEventListener('submit', e => {
+nameSearch.addEventListener('submit', async e => {
 	e.preventDefault();
+
+	// Get name from input field
 	const name = nameSearch.name.value.trim();
+
+	// Get country code of selected country
 	const country = document.querySelector('#country').value.slice(0, 2);
 
+	//Get data and handle results
 	searchByName(name, country)
 		.then(res => handleNameResult(res, name))
-		.catch(handleError);
+		.catch(renderError);
 });
 
 // Search by specific date when form is being submitted
 dateSearch.addEventListener('submit', e => {
 	e.preventDefault();
-	const inputDate = dateSearch.date.value.trim();
+
+	// Get date from input field and extract month and day
+	const inputDate = dateSearch.date.value;
 	const month = moment(inputDate).month() + 1;
 	const day = moment(inputDate).date();
+
+	// Get selected country and country code
 	const country = document.querySelector('#country').value;
 	const countryCode = country.slice(0, 2);
 
+	// Get data and handle results
 	searchByDate(month, day, countryCode)
 		.then(res => renderDateSearch(res, country))
-		.catch(handleError);
+		.catch(renderError);
 });
 
 // Render country list when page is being loaded
--- assets/script/nameday.js
+++ assets/script/nameday.js
@@ -1,23 +1,21 @@
 /*
- *
+ * International Nameday API
  *
  */
 
-const BASE_URL = 'https://api.abalin.net';
-
 // Fetch and return data
 const fetchData = async query => {
-	const response = await fetch(BASE_URL + query);
+	const response = await fetch('https://api.abalin.net' + query);
 	return await response.json();
 };
 
-// Search for nameday by name
+// Search for name day by name and country
 const searchByName = async (name, country = 'se') => {
 	const query = `/getdate?name=${name}&country=${country}`;
 	return await fetchData(query);
 };
 
-// Search for nameday by date
+// Search for name day by specific date and country
 const searchByDate = async (month, day, country = 'se') => {
 	const query = `/namedays?country=${country}&month=${month}&day=${day}`;
 	return await fetchData(query);
