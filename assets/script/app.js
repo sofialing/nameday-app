1792d4def61a79cd0e8bb2d1cda3273b6ac2fbe8
Sofia Ling <sofialing@live.se>
2020-01-13 19:36:26 +0100

    More refactoring

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,117 +1,56 @@
 /*
  * Name day search
  */
-const nameSearch = document.querySelector('#name-search');
-const dateSearch = document.querySelector('#date-search');
-const resultEl = document.querySelector('#nameday');
+const searchName = document.querySelector('#search-name');
+const searchDate = document.querySelector('#search-date');
+const searchToday = document.querySelector('#search-today');
+const searchResult = document.querySelector('#search-result');
 
 // Render a list of availabe countries
-const renderCountryList = () => {
-	const countryList = [
-		{
-			name: 'Austria',
-			code: 'at',
-			timeZone: 'Europe/Vienna'
-		},
-		{
-			name: 'Czechia',
-			code: 'cz',
-			timeZone: 'Europe/Prague'
-		},
-		{
-			name: 'Germany',
-			code: 'de',
-			timeZone: 'Europe/Berlin'
-		},
-		{
-			name: 'Denmark',
-			code: 'dk',
-			timeZone: 'Europe/Copenhagen'
-		},
-		{
-			name: 'Spain',
-			code: 'es',
-			timeZone: 'Europe/Madrid'
-		},
-		{
-			name: 'Finland',
-			code: 'fi',
-			timeZone: 'Europe/Helsinki'
-		},
-		{
-			name: 'France',
-			code: 'fr',
-			timeZone: 'Europe/Paris'
-		},
-		{
-			name: 'Croatia',
-			code: 'hr',
-			timeZone: 'Europe/Zagreb'
-		},
-		{
-			name: 'Hungary',
-			code: 'hu',
-			timeZone: 'Europe/Budapest'
-		},
-		{
-			name: 'Italy',
-			code: 'it',
-			timeZone: 'Europe/Rome'
-		},
-		{
-			name: 'Poland',
-			code: 'pl',
-			timeZone: 'Europe/Warsaw'
-		},
-		{
-			name: 'Sweden',
-			code: 'se',
-			timeZone: 'Europe/Stockholm'
-		},
-		{
-			name: 'Slovakia',
-			code: 'sk',
-			timeZone: 'Europe/Bratislava'
-		},
-		{
-			name: 'United States of America',
-			code: 'us',
-			timeZone: 'America/Toronto'
-		}
-	];
+const renderCountryList = async () => {
+	// Get list of supported countries
+	const countryList = await fetchData('../assets/script/countrylist.json');
 
 	// Append countries as options to select element
-	let html = countryList
-		.map(
-			country =>
-				`<option value="${country.code}-${country.timeZone}">${country.name}</option>`
-		)
+	const html = countryList
+		.map(country => {
+			return `<option value="${country.code}-${country.time}">${country.name}</option>`;
+		})
 		.join('');
 
-	document.querySelectorAll('select').forEach(list => (list.innerHTML = html));
+	document.querySelectorAll('select').forEach(el => (el.innerHTML = html));
 };
 
 // Handle error messages and render to page
 const renderError = message => {
-	resultEl.innerHTML = `<div class="alert alert-warning" role="alert">${message}</div>`;
+	searchResult.innerHTML = `<div class="alert alert-warning" role="alert">${message}</div>`;
 };
 
-// Handle search results for name search, format and set variables
+// Handle results for name search: format and set variables
 const handleNameResult = (res, search) => {
 	// Check if search got no results & render message
 	if (!res.results.length) {
-		renderError('No name day found.');
+		renderError(`No results for the name "${search}" in selected country.`);
 		return;
 	}
 
-	// Remove searched name from string of names (if multiple names)
-	const nameList = res.results[0].name
-		.split(', ')
+	// Check if searched name has an exact match in results
+	const name = res.results[0].name
+		.split(/[ ,]+/) // Reg ex to fix issue with .split(', ') not working on some countires
+		.filter(name => name.toLowerCase() === search.toLowerCase())
+		.toString();
+
+	// Render error message if searched name don't have an match
+	if (!name.length) {
+		renderError(`No results for the name "${search}" in selected country.`);
+		return;
+	}
+
+	// Remove searched name from string of names
+	const namelist = res.results[0].name
+		.split(/[ ,]+/) // Reg ex to fix issue with .split(', ') not working on some countires
 		.filter(name => name.toLowerCase() !== search.toLowerCase())
 		.join(', ');
-	const name = res.results[0].name
-		.split(', ')
-		.filter(name => name.toLowerCase() === search.toLowerCase());
 
 	// Format date of the name day
 	const date = moment()
@@ -122,9 +61,10 @@ const handleNameResult = (res, search) => {
 	// Get country name
 	const country = res['country code'];
 
-	renderNameSearch(name, nameList, date, country);
+	renderNameSearch(name, namelist, date, country);
 };
 
+// Handle results for date search: format and set variables
 const handleDateSearch = (res, country) => {
 	// Check if search got no results & render message
 	if (!res.data.length) {
@@ -137,17 +77,17 @@ const handleDateSearch = (res, country) => {
 		.date(res.data[0].dates.day)
 		.format('dddd, MMMM Do');
 
-	// Format names as a list
-	const nameList = res.data[0].namedays[country]
+	// Format names as a list item
+	const namelist = res.data[0].namedays[country]
 		.split(', ')
 		.map(name => `<li>${name}</li>`)
 		.join('');
 
-	renderDateSearch(date, nameList, country);
+	renderDateSearch(date, namelist, country);
 };
 
 // Render results of search by specific name
-const renderNameSearch = (name, nameList, date, country) => {
+const renderNameSearch = (name, namelist, date, country) => {
 	const html = `
 		<div class="card">
 			<div class="card-body text-center">
@@ -156,35 +96,35 @@ const renderNameSearch = (name, nameList, date, country) => {
 				<p class="date">${date}</p>
 				<p class="text-muted mb-0">
 					More on this day: 
-					<span class="name-list">${nameList.length ? nameList : 'no one'}</span>
+					<span class="name-list">${namelist.length ? namelist : 'no one'}</span>
 				</p>
 			</div>
 		</div>`;
-	resultEl.innerHTML = html;
+	searchResult.innerHTML = html;
 };
 
 // Render results of search by specific date
-const renderDateSearch = (date, nameList, country) => {
+const renderDateSearch = (date, namelist, country) => {
 	const html = `
 		<div class="card">
 			<div class="card-body text-center">
 				<img src="/assets/img/${country}.svg" class="flag-icon">
 				<h2 class="font-weight-bold">${date}</h2>
-				<ul class="name-list">${nameList}</ul>
+				<ul class="name-list">${namelist}</ul>
 			</div>
 		</div>`;
-	resultEl.innerHTML = html;
+	searchResult.innerHTML = html;
 };
 
 // Search by specific name when form is being submitted
-nameSearch.addEventListener('submit', async e => {
+searchName.addEventListener('submit', async e => {
 	e.preventDefault();
 
 	// Get name from input field
-	const name = nameSearch.name.value.trim();
+	const name = searchName.name.value.trim();
 
 	// Get country code of selected country
-	const country = document.querySelector('#country').value.slice(0, 2);
+	const country = document.querySelector('#search-country').value.slice(0, 2);
 
 	// Get data and handle results
 	searchByName(name, country)
@@ -193,16 +133,16 @@ nameSearch.addEventListener('submit', async e => {
 });
 
 // Search by specific date when form is being submitted
-dateSearch.addEventListener('submit', e => {
+searchDate.addEventListener('submit', e => {
 	e.preventDefault();
 
 	// Get date from input field and extract month and day
-	const inputDate = dateSearch.date.value;
+	const inputDate = searchDate.date.value;
 	const month = moment(inputDate).month() + 1;
 	const day = moment(inputDate).date();
 
 	// Get selected country and country code
-	const country = document.querySelector('#country').value.slice(0, 2);
+	const country = document.querySelector('#search-country').value.slice(0, 2);
 
 	// Get data and handle results
 	searchByDate(month, day, country)
@@ -211,12 +151,12 @@ dateSearch.addEventListener('submit', e => {
 });
 
 // Search for today's naming day when a country is selected
-document.querySelector('#today').addEventListener('change', e => {
-	const country = document.querySelector('#today').value.slice(0, 2);
-	const timeZone = document.querySelector('#today').value.slice(3);
+searchToday.addEventListener('change', () => {
+	const country = searchToday.value.slice(0, 2);
+	const timeZone = searchToday.value.slice(3);
 
 	// Get data and handle results
-	searchToday(country, timeZone)
+	searchByToday(country, timeZone)
 		.then(res => handleDateSearch(res, country))
 		.catch(renderError);
 });
@@ -225,6 +165,6 @@ document.querySelector('#today').addEventListener('change', e => {
 renderCountryList();
 
 // Render todays name day when page is being loaded
-searchToday()
+searchByToday()
 	.then(res => handleDateSearch(res, 'at'))
 	.catch(renderError);
--- assets/script/nameday.js
+++ assets/script/nameday.js
@@ -3,26 +3,28 @@
  *
  */
 
+const base_url = 'https://api.abalin.net';
+
 // Fetch and return data
-const fetchData = async query => {
-	const response = await fetch('https://api.abalin.net' + query);
+const fetchData = async url => {
+	const response = await fetch(url);
 	return await response.json();
 };
 
 // Search for name day by name and country
 const searchByName = async (name, country = 'se') => {
 	const query = `/getdate?name=${name}&country=${country}`;
-	return await fetchData(query);
+	return await fetchData(base_url + query);
 };
 
 // Search for name day by specific date and country
 const searchByDate = async (month, day, country = 'se') => {
 	const query = `/namedays?country=${country}&month=${month}&day=${day}`;
-	return await fetchData(query);
+	return await fetchData(base_url + query);
 };
 
 // Search for name days today for specified country and timezone
-const searchToday = async (country = 'at', timeZone = 'Europe/Vienna') => {
+const searchByToday = async (country = 'at', timeZone = 'Europe/Vienna') => {
 	const query = `/today?timezone=${timeZone}&country=${country}`;
-	return await fetchData(query);
+	return await fetchData(base_url + query);
 };
