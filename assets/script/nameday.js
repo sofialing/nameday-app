37c5cf96bdb539d430507b195ca31b6c2baf3336
Sofia Ling <sofialing@live.se>
2020-01-14 08:58:06 +0100

    Added support to get name days for yesterday and tomorrow

--- assets/script/nameday.js
+++ assets/script/nameday.js
@@ -6,25 +6,30 @@
 const base_url = 'https://api.abalin.net';
 
 // Fetch and return data
-const fetchData = async url => {
+const getData = async url => {
 	const response = await fetch(url);
+
+	if (!response.status === 200) {
+		throw new Error('Something went wrong. Try again later.');
+	}
+
 	return await response.json();
 };
 
 // Search for name day by name and country
-const searchByName = async (name, country = 'se') => {
+const getDataByName = async (name, country) => {
 	const query = `/getdate?name=${name}&country=${country}`;
-	return await fetchData(base_url + query);
+	return await getData(base_url + query);
 };
 
 // Search for name day by specific date and country
-const searchByDate = async (month, day, country = 'se') => {
+const getDataByDate = async (month, day, country) => {
 	const query = `/namedays?country=${country}&month=${month}&day=${day}`;
-	return await fetchData(base_url + query);
+	return await getData(base_url + query);
 };
 
 // Search for name days today for specified country and timezone
-const searchByToday = async (country = 'at', timeZone = 'Europe/Vienna') => {
-	const query = `/today?timezone=${timeZone}&country=${country}`;
-	return await fetchData(base_url + query);
+const getDataByDay = async (day, country, timeZone) => {
+	const query = `/${day}?timezone=${timeZone}&country=${country}`;
+	return await getData(base_url + query);
 };
